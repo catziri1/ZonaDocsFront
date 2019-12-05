231782509455e2e914e5eeee3db1b21d1633f2e2
Alan <Perez@Alans-MacBook-Pro.local>
2019-12-05 00:14:39 -0600

    Admin Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'admin', loadChildren: '../app/admin-section/admin-section.module#AdminSectionModule'},
+  {path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
