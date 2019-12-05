784db172468157a8c5ba793041a116ae8f031908
is703374@iteso.mx <is703374@iteso.mx>
2019-12-05 04:37:31 -0600

    getCategoriasPublicacion en servicio de notas

--- src/app/admin-section/crear-nota/crear-nota.component.ts
+++ src/app/admin-section/crear-nota/crear-nota.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { Component, OnInit, OnDestroy, AfterViewInit } from '@angular/core';
+import { NotasService } from '../notas.service';
+import { Categoria } from '../notas';
 
 
 @Component({
@@ -7,10 +8,21 @@ import { HttpClient } from '@angular/common/http'
   templateUrl: './crear-nota.component.html',
   styleUrls: ['./crear-nota.component.css']
 })
-export class CrearNotaComponent implements OnInit {
 
-  constructor(private httpClient:HttpClient) { }
+
+export class CrearNotaComponent implements OnInit {
+  categorias:Categoria= {
+    nombre: ''
+  };
+  constructor(    
+    private notasService:NotasService
+    ) { }
   ngOnInit() {
   }
-  
+  getCategoriasPublicion() {
+    this.notasService.getCategoriasPublicion().then(response => {
+      console.log('Response: ', response);
+      this.categorias = response;
+    });
+  }
 }
--- src/app/admin-section/notas.service.ts
+++ src/app/admin-section/notas.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotasService {
 
-  constructor() { }
+  constructor(private httpClient:HttpClient) { }
+  getCategoriasPublicion():Promise<any> {
+    const url = "https://zona-docs-api.herokuapp.com/categoriasPublicacion";
+    return this.httpClient.get(url).toPromise();
+  }
+
 }
