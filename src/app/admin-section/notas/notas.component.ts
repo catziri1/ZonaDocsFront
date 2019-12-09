e0f85c5df1790d2fd083d65dc9f8e587a2275220
luisacfl <luisa.fl.97@gmail.com>
2019-12-09 04:30:28 -0600

    select options periodista y categoria

--- src/app/admin-section/notas/notas.component.ts
+++ src/app/admin-section/notas/notas.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { NotasService } from '../notas.service';
-import { Nota } from '../nota';
+import { Nota, Categoria } from '../nota';
 import { Router, ActivatedRoute } from '@angular/router';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Periodista } from '../periodista';
 
 @Component({
   selector: 'app-notas',
@@ -9,20 +11,43 @@ import { Router, ActivatedRoute } from '@angular/router';
   styleUrls: ['./notas.component.css']
 })
 export class NotasComponent implements OnInit {
-
-  notas: Nota[] = [];
+  form: FormGroup;
+  periodistas: Periodista[];
+  categorias: Categoria[];
+  notas: Nota[];
 
   constructor(private notasService: NotasService,
               private router: Router,
-              private route: ActivatedRoute) {}
+              private route: ActivatedRoute,
+              private formBuilder:FormBuilder) {
+                this.form = this.formBuilder.group({
+                  categorias:[''],
+                  periodistas:['']
+                });
+
+              }
 
   ngOnInit() {
     this.getNotas();
+    this.getCategorias();
+    this.getPeriodistas();
+    console.log(this.categorias);
   }
   getNotas(): void {
     this.notasService.getNotas().then(notas => this.notas = notas);
   }
-  
+  submit(){
+    console.log(this.form.value);
+  }
+
+  getCategorias(){
+    return this.notasService.getCategoriasPublicacion().then(c => this.categorias = c);
+  }
+
+  getPeriodistas(){
+    return this.notasService.getPeriodistas().then(p => this.periodistas = p);
+  }
+
   /*mostrarDetalle(desapDetalle) {
     this.router.navigate([])
   }*/
